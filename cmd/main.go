077a8e89618a6cad06d6da0f9af487166f153156
Ochir.zha <ochir-zhamsuev@yandex.ru>
2025-04-01 17:41:55 +0800

    add grpc api

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"github.com/Muvi7z/chat-auth-s/gen/api/chat_v1"
+	"github.com/Muvi7z/chat-auth-s/internal/api/chat"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":50041")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	chat_v1.RegisterChatV1Server(s, chat.NewImplementationChat())
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
+}
--- internal/api/chat/connect_chat.go
+++ internal/api/chat/connect_chat.go
@@ -1,16 +1,51 @@
 package chat
 
-//func (i *ImplementationChat) ConnectChat(req *chat_v1.ConnectChatRequest, stream chat_v1.ChatV1_ConnectChatServer) error {
-//	i.mxChannels.RLock()
-//	chatChan, ok := i.channels[req.ChatId]
-//	i.mxChannels.RUnlock()
-//
-//	if !ok {
-//		return status.Errorf(codes.NotFound, "Chat with id %s not found", req.ChatId)
-//	}
-//
-//	i.mxChat.Lock()
-//	if _, okChat := i.channels[req.ChatId]; !okChat {
-//
-//	}
-//}
+import (
+	"github.com/Muvi7z/chat-auth-s/gen/api/chat_v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (i *ImplementationChat) ConnectChat(req *chat_v1.ConnectChatRequest, stream chat_v1.ChatV1_ConnectChatServer) error {
+	i.mxChannels.RLock()
+	chatChan, ok := i.channels[req.ChatId]
+	i.mxChannels.RUnlock()
+
+	if !ok {
+		return status.Errorf(codes.NotFound, "Chat with id %s not found", req.ChatId)
+	}
+
+	i.mxChat.Lock()
+	if _, okChat := i.chats[req.ChatId]; !okChat {
+		i.chats[req.GetChatId()] = &Chat{
+			streams: make(map[string]chat_v1.ChatV1_ConnectChatServer),
+		}
+	}
+	i.mxChat.Unlock()
+
+	i.chats[req.GetChatId()].m.Lock()
+	i.chats[req.GetChatId()].streams[req.GetUsername()] = stream
+	i.chats[req.GetChatId()].m.Unlock()
+
+	for {
+		select {
+		case msg, okCh := <-chatChan:
+			if !okCh {
+				return nil
+			}
+
+			for _, st := range i.chats[req.GetChatId()].streams {
+				if err := st.Send(msg); err != nil {
+					return err
+				}
+
+			}
+		case <-stream.Context().Done():
+			i.chats[req.GetChatId()].m.Lock()
+			delete(i.chats[req.GetChatId()].streams, req.GetUsername())
+			i.chats[req.GetChatId()].m.Unlock()
+			return nil
+		}
+	}
+
+}
